81a8bc9b2d8c4f90117ae9ae06f4b24d54f780ef
HomePC <slider.87@mail.ru>
2017-03-15 20:57:24 +0300

    Zada4a2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,16 @@
 package com.company;
 
+
 class Zada4a {
     public static void main(String[] args) {
-        int a=4;
-        if (a%2==0) {
-            System.out.println("" + a + " число четное");
-        }
-        else {
-            System.out.println("" + a + " число нечетное");
-        }
-    }
+        int a = 8;
+        String Type = (a % 2 == 0) ? "" + a + "-четное число" : "" + a + "-нечетное число";
 
+        System.out.println(Type);
+
+    }
 }
+
+
+
+
