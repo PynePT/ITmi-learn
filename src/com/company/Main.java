33c47aa977f697dbf0fb05864994592b4cbb72b3
HomePC <slider.87@mail.ru>
2017-03-15 20:00:35 +0300

    Zada4a1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+class Zada4a {
+    public static void main(String[] args) {
+        int a=4;
+        if (a%2==0) {
+            System.out.println("" + a + " число четное");
+        }
+        else {
+            System.out.println("" + a + " число нечетное");
+        }
+    }
+
+}
