550e208960fd2b4a067ea24e8a8cd0f3f7060ae3
HomePC <slider.87@mail.ru>
2017-03-15 21:41:20 +0300

    Zada4a3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,33 @@
 package com.company;
 
 
+
+
 class Zada4a {
     public static void main(String[] args) {
-        int a = 8;
-        String Type = (a % 2 == 0) ? "" + a + "-четное число" : "" + a + "-нечетное число";
+        double m=8.45;
+        double n=10.15;
+        double x=10;
 
-        System.out.println(Type);
+       double a = Math.abs(x-m);
+       double b = Math.abs(x-n);
+       if (a==b){
+           System.out.println(""+b+"и"+a+" равноудалены друг к другу");
+       }
+
+       if(a>b) {
+           System.out.println("" + a + "ближе,чем " + b + "");
+       }
+
+
+       if(b>a){
+
+               System.out.println(""+b+" ближе,чем "+a+"");
+           }
+       }
 
     }
-}
+
 
 
 
