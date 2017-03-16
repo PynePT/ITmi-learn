c4ae5c591d10acb4b5f1a2e3a23e093bac785836
HomePC <slider.87@mail.ru>
2017-03-16 23:55:15 +0300

    Zada4a3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,9 @@ package com.company;
 
 class Zada4a {
     public static void main(String[] args) {
-        double m=1;
-        double n=1;
+        double m=9;
+        double n=9;
         double x=10;
-
        double a = Math.abs(x-m);
        double b = Math.abs(x-n);
        if (a==b){
